b7216d84c89af0bbd1e5f05779851948e0251f41
liuyi <327782437@qq.com>
2015-12-25 09:32:54 +0800

    insert a demo

---
+++ demo/tutorial/app/controllers/UpController.php
@@ -0,0 +1,30 @@
+<?php
+class UpController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+    
+    }
+    public function registerAction()
+    {
+
+        $user = new User();
+
+        // Store and check for errors
+        $success = $user->save($this->request->getPost(), array('name', 'email'));
+
+        if ($success) {
+            echo "Thanks for registering!";
+        } else {
+            echo "Sorry, the following problems were generated: ";
+            foreach ($user->getMessages() as $message) {
+                echo $message->getMessage(), "<br/>";
+            }
+        }
+
+        $this->view->disable();
+    }
+
+}
+?>
